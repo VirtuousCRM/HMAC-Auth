46aeb12b74c3b254e80694bf09656f59a8ae2f85
Adam Valverde <adam.dorado@gmail.com>
2016-11-15 16:08:06 -0700

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # HMAC-Auth
-An example of how to authenticate to the Virtuous API via HMAC.
+
+The Virtuous API can be authenticated in two different ways, OAuth which is mainly used for User Authentication and HMAC which is used for external entities providing information to Nonprofits on Virtuous.
+
+This code sample is an example of how to sign your request with the HMAC authentication pattern used in Virtuous.
+
+Contact Virtuous at info@virtuoussoftware.com to request a Private Key to get started.
---
+++ VirtuousHmacAuth/VirtuousHmacAuth/VirtuousHmacAuth/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Net.Http;
+
+namespace VirtuousHmacAuth
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var virtuousApi = "https://api.virtuoussoftware.com";
+
+            var applicationKey = "REPLACE_ME";
+
+            var requestMethod = "POST";
+            var requestUrl = "/api/gift/transaction/";
+
+            var virtuousHmacAuth = new VirtuousHmacAuth();
+            var hmacHeader = virtuousHmacAuth.GenerateHmacHeader(applicationKey, requestMethod, requestUrl);
+
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri(virtuousApi);
+            client.DefaultRequestHeaders.Add("Authorization", hmacHeader);
+            client.DefaultRequestHeaders.Add("Content-Type", "application/json");
+
+            StringContent postData = new StringContent("{ id: \"Some Json Object\" }");
+
+            var postAsync = client.PostAsync(requestUrl, postData);
+            postAsync.RunSynchronously();
+            var result = postAsync.Result;
+
+            Console.WriteLine("Status Code: {0}", result.StatusCode);
+            Console.WriteLine("Content: {0}", result.Content);
+        }
+    }
+}
